d95f1f7890c3747550088e68cecd1d9d798b357f
tiemyz <rm95145@fiap.com.br>
2022-09-16 21:50:36 -0300

    cp finalizado.

--- receitas/src/main/java/br/com/fiap/PrimaryController.java
+++ receitas/src/main/java/br/com/fiap/PrimaryController.java
@@ -1,12 +1,75 @@
 package br.com.fiap;
 
-import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
-public class PrimaryController {
+public class PrimaryController implements Initializable{
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    @FXML private TextField textFieldTitulo;
+    @FXML private TextField textFieldIngredientes;
+    @FXML private TextField textFieldModo;
+    @FXML private TextField textFieldCusto;
+    @FXML private TextField textFieldCategoria;
+    @FXML private ChoiceBox<String> choiceBox;
+    @FXML private ListView<Receitas> listView;
+    @FXML private ListView<Receitas> listViewReceitasFavoritadas;
+
+    private List<Receitas> lista = new ArrayList<>();
+    private List<Receitas> listaReceitasFavs= new ArrayList<>();
+    String[] listaFavs = {"SIM", "NÃO"};
+
+    public void salvar() {
+
+        String titulo = textFieldTitulo.getText();
+        String ingredientes = textFieldIngredientes.getText();
+        String modo = textFieldModo.getText();
+        Double custo = Double.valueOf(textFieldCusto.getText());
+        String categoria = textFieldCategoria.getText();
+        String favoritadas = choiceBox.getValue();
+      
+        Receitas receitas = new Receitas(titulo, ingredientes, modo, (double) custo, categoria, favoritadas);
+        if (favoritadas == "SIM") {
+            listaReceitasFavs.add(receitas);
+        }
+        lista.add(receitas);
+        atualizarList();
+    }
+
+    public void ordenarCategoria(){
+        lista.sort((o1, o2) -> o1.getCategoria().compareTo(o2.getCategoria()));
+        atualizarList();
+    }
+
+
+    public void ordenarCusto(){
+        lista.sort((o1, o2) -> Double.compare(o1.getCusto(), o2.getCusto()) );
+        atualizarList();
+    }
+
+
+    public void ordenarTitulo(){
+        lista.sort((o1, o2) -> o1.getTitulo().compareTo(o2.getTitulo()));
+        atualizarList();
+    }
+
+    public void atualizarList(){
+
+        listView.getItems().clear();
+        listView.getItems().addAll(lista);
+        listViewReceitasFavoritadas.getItems().clear();
+        listViewReceitasFavoritadas.getItems().addAll(listaReceitasFavs);
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        choiceBox.getItems().addAll(listaFavs);
     }
 }
--- receitas/src/main/java/br/com/fiap/Receitas.java
+++ receitas/src/main/java/br/com/fiap/Receitas.java
@@ -1,3 +1,79 @@
-public class Receitas {
-    
+package br.com.fiap;
+
+
+public class Receitas implements Comparable {
+
+    private String titulo;
+    private String ingredientes;
+    private String modo;
+    private Double custo;
+    private String categoria;
+    private String favoritadas;
+
+    public Receitas(String titulo, String ingredientes, String modo, Double custo, String categoria, String favoritadas) {
+        this.titulo = titulo;
+        this.ingredientes = ingredientes;
+        this.modo = modo;
+        this.custo = custo;
+        this.categoria = categoria;
+        this.favoritadas = favoritadas;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(String ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    public String getModo() {
+        return modo;
+    }
+
+    public void setModo(String modo) {
+        this.modo = modo;
+    }
+
+    public Double getCusto() {
+        return custo;
+    }
+
+    public void setCusto(Double custo) {
+        this.custo = custo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getFavoritadas() {
+        return favoritadas;
+    }
+
+    public void setFavoritadas(String favoritadas) {
+        this.favoritadas = favoritadas;
+    }
+
+    @Override
+    public String toString() {
+        return "Título da receita:" + titulo + "\nIngredientes:" + ingredientes + "\nModo de preparo:" + modo + "\nR$" + custo + "\nCategoria:" + categoria;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
 }
